fc86a9696e86756110f0a6e5652cafb171e66594
rhl120 <rhl120@protonmail.com>
2022-08-28 17:02:46 +0200

    Remove trailing lines

--- blinker.c
+++ blinker.c
@@ -183,7 +183,7 @@ static int blinker_device_init(struct blinker_device_struct *dev,
 	cdev_init(&dev->cdev, &fops);
 	if(!(ret = cdev_add(&dev->cdev, dev_num, 1)))
 		goto ret;
-gpio_free_ret: 
+gpio_free_ret:
 #ifndef NO_GPIO
 	gpio_free(gpio_pin);
 #endif
